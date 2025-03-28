57af44c3e2db3eaa0e37bc1cf21f7f1101eb6bc9
Cezou <cesairepro@gmail.com>
2025-03-28 17:39:33 +0100

    fix ghpages try

--- docs/src/pages/Documentation.js
+++ docs/src/pages/Documentation.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
